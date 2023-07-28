0c383133d937bc60ce1d21f3fd10d8aed75de39a
BusinessJoe <lukasboelling002@gmail.com>
2023-07-27 21:07:28 -0400

    Run prettierd on save in .js and .ts files

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,7 +5,7 @@ local lsp = require('lsp-zero').preset({})
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	['<C-Space>'] = cmp.mapping.complete(),
@@ -25,6 +25,8 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+
 end)
 
 
@@ -51,6 +53,5 @@ require('lspconfig').lua_ls.setup {
 		},
 	},
 }
-
 
 lsp.setup()
